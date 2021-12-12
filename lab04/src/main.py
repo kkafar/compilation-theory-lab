2b281bf683d723cf3846bb16ed31aed9cd65d9a9
Kacper Kafara <kacperkafara@gmail.com>
2021-12-12 20:32:48 +0100

    [lab4] Adjust main

--- lab04/src/main.py
+++ lab04/src/main.py
@@ -1,22 +1,20 @@
 
 import sys
-import ply.yacc as yacc
 import Mparser
 import scanner
-from TreePrinter import TreePrinter
 from TypeChecker import TypeChecker
 
 if __name__ == '__main__':
 
     try:
         filename = sys.argv[1] if len(sys.argv) > 1 else "data/example1.m"
-        file = open(filename, "r")
+        with open(filename) as file:
+            text = file.read()
     except IOError:
         print("Cannot open {0} file".format(filename))
         sys.exit(0)
 
     parser = Mparser.parser
-    text = file.read()
 
     ast = parser.parse(text, lexer=scanner.lexer)
 
