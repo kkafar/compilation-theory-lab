d189b50906b4a104b34d788b2db54f7a058a4a1c
Kacper Kafara <kacperkafara@gmail.com>
2021-12-12 23:31:13 +0100

    Implement SymbolTable v1

--- lab04/src/SymbolTable.py
+++ lab04/src/SymbolTable.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python
 
+class Symbol(object):
+    pass
+
 
 class VariableSymbol(Symbol):
-
-    def __init__(self, name, type):
-        pass
-    #
-
-
-class SymbolTable:
-
-    def __init__(self, parent=None, name='global'): # parent scope and symbol table name
-        pass
-    #
-
-    def put(self, name, symbol): # put variable symbol or fundef under <name> entry
-        pass
-    #
-
-    def get(self, name): # get variable symbol or fundef from <name> entry
-        pass
-    #
-
-    def getParentScope(self):
-        pass
-    #
-
-    def pushScope(self, name):
-        pass
-    #
-
-    def popScope(self):
-        pass
-    #
+    def __init__(self, name, _type):
+        self.name = name
+        self.type = _type
 
 
+class SymbolTable(object):
+    def __init__(self, parent=None, name='global'):  # parent scope and symbol table name
+        self.symbols = {}
+        self.parent_scope = parent
+        self.scope_name = name
+        self.scope = 0
+
+    def put(self, name, symbol):  # put variable symbol or fundef under <name> entry
+        self.symbols[name] = symbol
+
+    def get(self, name):  # get variable symbol or fundef from <name> entry
+        return self.symbols[name]
+
+    def get_parent_scope(self):
+        return self.parent_scope
+
+    def push_scope(self, name):
+        self.scope += 1
+
+    def pop_scope(self):
+        if self.scope == 0:
+            raise RuntimeError("Can not decrease scope!")
+        self.scope -= 1
