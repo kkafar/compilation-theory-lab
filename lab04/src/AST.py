1a9e87dfe241560425b3a6004b7a249f711afa48
Kacper Kafara <kacperkafara@gmail.com>
2021-12-12 20:35:03 +0100

    [lab4] Add calls to super init method in AST class (suggested by     PyCharm)

--- lab04/src/AST.py
+++ lab04/src/AST.py
@@ -18,31 +18,37 @@ class Node(object):
 
 class Program(Node):
     def __init__(self, instructions=None):
+        super().__init__()
         self.instructions = instructions
 
 
 class IntNum(Node):
     def __init__(self, value):
+        super().__init__()
         self.value = value
 
 
 class FloatNum(Node):
     def __init__(self, value):
+        super().__init__()
         self.value = value
 
 
 class StringValue(Node):
     def __init__(self, string):
+        super().__init__()
         self.string = string
 
 
 class Variable(Node):
     def __init__(self, name):
+        super().__init__()
         self.name = name
 
 
 class BinExpr(Node):
     def __init__(self, op, left, right):
+        super().__init__()
         self.op = op
         self.left = left
         self.right = right
@@ -52,6 +58,7 @@ class BinExpr(Node):
 
 class RelopExpr(Node):
     def __init__(self, op, left, right):
+        super().__init__()
         self.op = op
         self.left = left
         self.right = right
@@ -61,12 +68,14 @@ class RelopExpr(Node):
 
 class UnaryExpr(Node):
     def __init__(self, operator, operand):
+        super().__init__()
         self.operator = operator
         self.operand = operand
 
 
 class RelExpr(Node):
     def __init__(self, op, left, right):
+        super().__init__()
         self.op = op
         self.left = left
         self.right = right
@@ -74,6 +83,7 @@ class RelExpr(Node):
 
 class Instructions(Node):
     def __init__(self, instruction):
+        super().__init__()
         if hasattr(instruction, '__len__'):
             self.instructions = instruction
         else:
@@ -82,6 +92,7 @@ class Instructions(Node):
 
 class Assignment(Node):
     def __init__(self, op, left, right):
+        super().__init__()
         self.op = op
         self.left = left
         self.right = right
@@ -89,12 +100,14 @@ class Assignment(Node):
 
 class Function(Node):
     def __init__(self, function, expressions):
+        super().__init__()
         self.function = function
         self.expressions = expressions
 
 
 class Conditional(Node):
     def __init__(self, condition, if_instruction, else_instruction=None):
+        super().__init__()
         self.condition = condition
         self.if_instruction = if_instruction
         self.else_instruction = else_instruction
@@ -102,6 +115,7 @@ class Conditional(Node):
 
 class Vector(Node):
     def __init__(self, value=None):
+        super().__init__()
         if value is None:
             self.values = []
         else:
@@ -110,21 +124,25 @@ class Vector(Node):
 
 class JumpStatement(Node):
     def __init__(self, statement):
+        super().__init__()
         self.statement = statement
 
 
 class PrintStatement(Node):
     def __init__(self, expressions):
+        super().__init__()
         self.expressions = expressions
 
 
 class ReturnStatement(Node):
     def __init__(self, expression=None):
+        super().__init__()
         self.expression = expression
 
 
 class Expressions(Node):
     def __init__(self, expression=None):
+        super().__init__()
         if expression is not None:
             self.expressions = [expression]
         else:
@@ -133,12 +151,14 @@ class Expressions(Node):
 
 class WhileLoop(Node):
     def __init__(self, expression, instruction):
+        super().__init__()
         self.expression = expression
         self.instruction = instruction
 
 
 class ForLoop(Node):
-    def __init__(self, variable, range_value_left,  range_value_right,  instruction):
+    def __init__(self, variable, range_value_left, range_value_right, instruction):
+        super().__init__()
         self.variable = variable
         self.range_value_left = range_value_left
         self.range_value_right = range_value_right
@@ -147,6 +167,7 @@ class ForLoop(Node):
 
 class Slice(Node):
     def __init__(self, matrix, vector):
+        super().__init__()
         self.matrix = matrix
         self.vector = vector
 
@@ -156,5 +177,4 @@ class Empty(Node):
 
 
 class Error(Node):
-    def __init__(self):
-        pass
+    pass
