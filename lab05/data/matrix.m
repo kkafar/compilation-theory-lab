33416a85e9f7d1d56ce6f2b80b14d00d8112e7db
Kacper Kafara <kacperkafara@gmail.com>
2022-01-14 14:44:37 +0100

    Declutter data/matrix.m

--- lab05/data/matrix.m
+++ lab05/data/matrix.m
@@ -9,18 +9,18 @@ D[1:3, 2:4] = 7;
 print D;
 print D[1:3, 2:4];
 print D[0, 0];
-
-E = D + 1;
-print E;
-
-V = [1, 2, 3];
-
-print V[0];
-print V[1:2];
-print V[V[1:2]];
-
 print D[0:1, 0:1];
 
 D[0, 0] = 1;
 
 print D[D[0:1, 0:1], 0];
+index = 1;
+print D[D[0:1, 0:index], 0];
+
+E = D + 1;
+print E;
+
+V = [1, 2, 3];
+print V[0];
+print V[1:2];
+print V[V[1:2]];
